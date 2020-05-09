97af1253288e9de6ba50f331b73763ac6126e597
aiandrews1111 <63754861+aiandrews1111@users.noreply.github.com>
2020-05-09 14:45:03 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+var canvas = document.getElementbyId('Game');
+var ctx = canvas.getContext('2d');
+var playerX = 175;
+var playerY = 175;
+
+
+var PlayerStats = {
+
+  HP:100,
+  Equppied:"Pistol",
+  Speed: 10,
+  Level: 1,
+  Name: "NA",
+};
+
+ddocument.addEventListener('keydown', event => {
+  if (event.keyCode === 87 || event.keyCode === 38) {
+    playerY = playerY - 2;
+    //W key or Up key
+  } else if (event.keyCode === 65 || event.keyCode === 37) {
+    playerX = playerX - 2;
+    //A Key or Left key
+  } else if (event.keyCode === 83 || event.keyCode === 40) {
+    playerY = playerY + 2;
+    //S Key or Down key
+  } else if (event.keyCode === 68 || event.keyCode === 39) {
+    playerX = playerX + 2;
+    //D Key or Right key
+  }
+});
+                          
+function updatePlayer(x, y){
+  ctx.fillStyle = "rgb(204, 0, 0)";
+  ctx.fillRect(x, y, 50, 50);
+}
+
+
+function update(){
+  updatePlayer(playerX, playerY);
+}
+
+setInterval(update, 15);
